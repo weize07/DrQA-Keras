229c0b4a8d795c5ed797fadcd4c3b95627cd4b58
weize07 <ze.wei1989@gmail.com>
2019-02-08 21:20:40 +0800

    change model

--- drqa/reader/transformer_reader.py
+++ drqa/reader/transformer_reader.py
@@ -6,23 +6,63 @@ from hdf5_util import *
 from drqa.reader import utils, vector, config, data, DataGenerator
 from keras.utils import to_categorical
 from keras.models import Model
-from keras.layers import Input, Dense, merge
+from keras.layers import Input, Dense, Multiply, Layer
+from keras import regularizers, constraints, initializers
+import keras.backend as K 
+
+class Attention(Layer):
+    '''
+    compute the 'attention' scores for question over ctx, 
+    which is a_i = exp(c_i*W*q) / sum_over_j(exp(c_j*W*q))
+    W is a weight matrix to be learned.
+    '''
+    def __init__(self, ctx_dim, ctx_length, **kwargs):
+        self.ctx_dim = ctx_dim
+        self.ctx_length = ctx_length
+        self.init = initializers.get('glorot_uniform')
+        super(Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        # input_shape: [ctx, question]; ctx: vectors of tokens, question: vector of question
+        self.W = self.add_weight((self.ctx_dim, self.ctx_dim,),
+                                 initializer=self.init,
+                                 name='{}_W'.format(self.name),
+                                 regularizer=regularizers.l2(0.01),
+                                 constraint=constraints.max_norm(2.))
+        super(Attention, self).build(input_shape)
+
+    def call(self, x, mask=None):
+        print(x.shape)
+        tmp = K.dot(K.transpose(x[0]), self.W)
+        tmp = K.dot(tmp, x[1])
+        a = K.exp(tmp)
+        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+        return a
+
+    def compute_output_shape(self, input_shape):
+        return self.ctx_dim
+
 
 def build_model(ctx_dim, ctx_length, which_side):
     """
     softmax(CTX * Ws * Q), Ws/e: matrix for start/end injection 
     which_side: start/end
     """
+    # inputs = Input(shape=(2,))
     inputs_ctx = Input(shape=(ctx_dim, ctx_length))
     inputs_que = Input(shape=(ctx_dim, 1))
 
-    # ctx_multi_mat = Dense(ctx_dim, activation='linear')(inputs_ctx)
-    que_multi_mat = Dense(ctx_dim, activation='linear')(inputs_ctx)
-    # ctx_mat_que = merge([ctx_multi_mat, inputs_que], output_shape=ctx_length, mode='mul')
-    ctx_mat_que = merge([inputs_ctx, que_multi_mat], output_shape=ctx_length, mode='mul')
+    # # ctx_multi_mat = Dense(ctx_dim, activation='linear')(inputs_ctx)
+    que_multi_mat = Dense(ctx_dim, activation='linear')(inputs_que)
+    # # ctx_mat_que = merge([ctx_multi_mat, inputs_que], output_shape=ctx_length, mode='mul')
+    ctx_mat_que = Multiply()([inputs_ctx, que_multi_mat])
 
     output = Dense(ctx_dim, activation='softmax', name=('%s_probs' % which_side))(ctx_mat_que)
+    # output = Attention(ctx_dim, ctx_length, name=('%s_probs' % which_side))(inputs)
+    # model = Model(input=inputs, output=output)
     model = Model(input=[inputs_ctx, inputs_que], output=output)
+    model.compile(loss='crossentropy', optimizer='adam', metrics=['accuracy'])
+
     return model
 
 def train_network(dataset):
@@ -58,11 +98,11 @@ if __name__ == '__main__':
     Y = []
     for qid in dataset['qas']:
         cid = dataset['qas'][qid]['cid']
-        print(dataset['qas'][qid]['bert_features'][0].shape)
-        print(dataset['contexts'][cid]['bert_features'].shape)
+        # print(dataset['qas'][qid]['bert_features'][0].shape)
+        # print(dataset['contexts'][cid]['bert_features'].shape)
         x = np.array([dataset['contexts'][cid]['bert_features'], dataset['qas'][qid]['bert_features'][0]])
         print(x.shape)
-        print(x)
+        # print(x)
         X.append(x)
         Y.append(to_categorical(dataset['qas'][qid]['answer_offsets'][0], num_classes=512))
         break
